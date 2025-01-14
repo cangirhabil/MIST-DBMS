1f6a618d35d4ac5c55ebe11f2aeb7bb045a54dfc
cangirhabil <hblcngr1400@gmail.com>
2025-01-14 23:14:14 +0300

    Refactor AuthGuard logic to prioritize user redirection based on authentication status

--- frontend/src/components/auth/AuthGuard.tsx
+++ frontend/src/components/auth/AuthGuard.tsx
@@ -10,11 +10,12 @@ export function AuthGuard({ children }: { children: React.ReactNode }) {
   const user = useAuthStore((state) => state.user)
 
   useEffect(() => {
-    if (user?.id === 'cm5wwbr0t000inz0f1m6jniww') {
-      router.replace(ROUTE_PATHS.ADMIN_STATISTICS)
-    } else if (user === null) {
+    if (user === null) {
       router.replace(ROUTE_PATHS.HOME)
-    } else {
+    }
+    else if (user?.id === 'cm5wwbr0t000inz0f1m6jniww') {
+      router.replace(ROUTE_PATHS.ADMIN_STATISTICS)
+    }else {
       router.replace(ROUTE_PATHS.MY_LISTS)
     }
   }, [user, router])
